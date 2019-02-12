c7a41fc229b97cad57ed245e9131abe4d65dbf7e
Swamy Biru <Swamy-Biru@gap.com>
2019-02-11 22:09:39 -0800

    DevOps-1[Swamy] First Commit of Project All Files

---
+++ jenkins-docker-git/src/main/java/com/swamy/jenkinsdockergit/JenkinsDockerGitApplication.java
@@ -0,0 +1,14 @@
+package com.swamy.jenkinsdockergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerGitApplication.class, args);
+	}
+
+}
+
